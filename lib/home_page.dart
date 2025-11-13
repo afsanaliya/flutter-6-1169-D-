c9ddd0ae9312888532aeffb42f5fe4b2c6fc9c1e
afsanaliya <afsanaliya214@gmail. com>
2025-11-13 12:03:14 +0600

    image,link

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         // leading: Icon(Icons.home),
-        backgroundColor: const Color.fromARGB(255, 226, 87, 159),
+        backgroundColor: const Color.fromARGB(255, 226, 87, 103),
         foregroundColor: const Color.fromARGB(255, 223, 170, 170),
         title: Text("My Flutter Project"),
         actions: [
@@ -80,9 +80,28 @@ class HomePage extends StatelessWidget {
       ],
       ),
 
-      body: Text(
-        "Liya",
-        style: GoogleFonts.lobster(textStyle: TextStyle(fontSize: 30)),
+       body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome to homepage",
+                style: GoogleFonts.lobster(textStyle: TextStyle(fontSize: 30)),
+              ),
+              Image.asset('assets/images/nature.jpg', height: 400),
+              Image.asset(
+                'assets/images/nature2.jpg',
+                height: 400,
+                width: 400,
+                fit: BoxFit.fill,
+              ),
+              Image.network(
+                'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
